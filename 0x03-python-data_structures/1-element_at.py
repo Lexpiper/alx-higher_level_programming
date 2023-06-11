e84002d592e6395d0074e068895ffaa1f4b670ea
Lexpiper <lexpiperfx@gmail.com>
2023-06-11 22:55:56 +0100

    retrieves an element from a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    list_len = len(my_list)
+
+    if idx < 0:
+        return none
+
+    elif idx > list_len - 1:
+        return none
+
+    return(my_list[idx])
