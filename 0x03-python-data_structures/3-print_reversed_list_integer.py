7d61ec21c09eecf8c537d27d5abd2c6d03e81b8a
Lexpiper <lexpiperfx@gmail.com>
2023-06-13 07:11:05 +0100

    reverse string

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,9 @@
+#!/bin/usr/python3
+
+def print_reversed_list_integer(my_list=[]):
+    if my_list:
+        list_len = len(my_list) - 1
+
+        while list_len >= 0:
+            print("{:d}".format(my_list[list_len]))
+            list_len -= 1
